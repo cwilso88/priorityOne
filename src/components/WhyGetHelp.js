cc435a17bdcfaaa650c2562871305d77818b7871
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2021-08-16 08:46:40 -0400

    Centered img line_600px and resized the img

--- src/components/WhyGetHelp.js
+++ src/components/WhyGetHelp.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
-import line_1000px from '../logos/line_1000px.png'
+import line_600px from '../logos/line_600px.png'
+
 
 
 
@@ -11,7 +12,7 @@ class WhyGetHelp extends React.Component {
             <section id="whygethelp">
                 <div className="whygethelp_header">
                     <h1 className="whygethelp_title">Focus on Business Issues Instead of User Issues</h1>
-                    <img src={line_1000px} alt="Gold underline" />
+                    <img src={line_600px} alt="Gold underline" />
 
                     <p>
                     Help desk services can be beneficial to companies of all sizes. Companies that have their own internal IT staff can use help desk services to outsource support for the solving of end-user issues, which are typically repetitive with a limited need for advanced technical expertise.
@@ -29,13 +30,13 @@ class WhyGetHelp extends React.Component {
                     <div className="whygethelp_checklist_container row">
                         <ul className="column">
                             <li><FontAwesomeIcon className="icon" icon={ faCheckCircle } />Does your IT staff get bogged down fixing employee issues?</li>
-                            <li><FontAwesomeIcon className="icon" icon={ faCheckCircle } />Are members of your IT staff pulled off important company initiatives to deal with user complaints?</li>
+                            <li><FontAwesomeIcon className="icon" icon={ faCheckCircle } />Is IT staff pulled off important tasks to deal with user complaints?</li>
                             <li><FontAwesomeIcon className="icon" icon={ faCheckCircle } />Are your users dissatisfied with response times from the IT team?</li>
                         </ul>
                         <ul className="column">
                             <li><FontAwesomeIcon className="icon" icon={ faCheckCircle } />Are your IT costs escalating?</li>
                             <li><FontAwesomeIcon className="icon" icon={ faCheckCircle } />Is your IT staff stretched too thin?</li>
-                            <li><FontAwesomeIcon className="icon" icon={ faCheckCircle } />Are outages and slow networks hampering your employee’s productivity?</li>
+                            <li><FontAwesomeIcon className="icon" icon={ faCheckCircle } />Are slow networks hampering your employee’s productivity?</li>
                         </ul>
                     </div>
                 </div>

32d2b35107f4669aa9ca1903770ca35f2753e472
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2021-08-19 23:16:49 -0400

    Setup contactus topimg comp

---
+++ src/components/ContactUsTopimg.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ContactUsTopimg extends React.Component {
+    render() {
+        return(
+            <div id="contactus_topimg">
+                <h1>Contact Priority One</h1>
+                <p>Get in Touch</p>
+            </div>
+        )
+    }
+}
+
+export default ContactUsTopimg;
